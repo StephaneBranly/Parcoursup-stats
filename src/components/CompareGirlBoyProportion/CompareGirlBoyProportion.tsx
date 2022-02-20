46c5092c8476fb58bea73141f155b887b626b6b6
Stephane_Branly <stephanebranly.pro@gmail.com>
2022-02-20 13:16:02 +0100

    [~] Finaliastion of proportion comparaison

--- src/components/CompareGirlBoyProportion/CompareGirlBoyProportion.tsx
+++ src/components/CompareGirlBoyProportion/CompareGirlBoyProportion.tsx
@@ -8,6 +8,7 @@ import {
     Tooltip,
     XAxis,
     YAxis,
+    LabelList,
 } from 'recharts'
 
 export interface CompareGirlBoyProportionProps {
@@ -17,45 +18,129 @@ export interface CompareGirlBoyProportionProps {
 
 const CompareGirlBoyProportion = (props: CompareGirlBoyProportionProps) => {
     const { data } = props
+
+    const createLabel = (value: number, pourcentage: number | string) => {
+        return `${pourcentage}% (${value})`
+    }
+
+    const computedData = data.map((entry) => {
+        const nbCandidates =
+            entry['count_candidates_boys'] + entry['count_candidates_girls']
+        const count_candidates_boys_pct = (
+            (entry['count_candidates_boys'] / nbCandidates) *
+            100
+        ).toFixed(0)
+        const count_candidates_girls_pct = (
+            (entry['count_candidates_girls'] / nbCandidates) *
+            100
+        ).toFixed(0)
+        const nbAccepted =
+            entry['count_accepted_boys'] + entry['count_accepted_girls']
+        const count_accepted_boys_pct = (
+            (entry['count_accepted_boys'] / nbAccepted) *
+            100
+        ).toFixed(0)
+        const count_accepted_girls_pct = (
+            (entry['count_accepted_girls'] / nbAccepted) *
+            100
+        ).toFixed(0)
+
+        return {
+            count_candidates_boys_pct,
+            count_candidates_girls_pct,
+            count_accepted_boys_pct,
+            count_accepted_girls_pct,
+            label_candidates_boys: createLabel(
+                entry['count_candidates_boys'],
+                count_candidates_boys_pct
+            ),
+            label_candidates_girls: createLabel(
+                entry['count_candidates_girls'],
+                count_candidates_girls_pct
+            ),
+            label_accepted_boys: createLabel(
+                entry['count_accepted_boys'],
+                count_accepted_boys_pct
+            ),
+            label_accepted_girls: createLabel(
+                entry['count_accepted_girls'],
+                count_accepted_girls_pct
+            ),
+            ...entry,
+        }
+    })
+
     return (
         <StatsCard title={props.title}>
             <ResponsiveContainer width="100%" height={250}>
-                <BarChart
-                    width={500}
-                    height={300}
-                    data={data}
-                    margin={{
-                        top: 20,
-                        right: 30,
-                        left: 20,
-                        bottom: 5,
-                    }}
-                >
+                <BarChart data={computedData}>
                     <CartesianGrid strokeDasharray="3 3" />
                     <XAxis dataKey="formationName" />
                     <YAxis />
                     <Tooltip />
-                    <Legend />
+                    <Legend
+                        iconSize={10}
+                        layout="horizontal"
+                        verticalAlign="bottom"
+                        align="center"
+                    />
+
                     <Bar
-                        dataKey="count_candidates_boys"
+                        isAnimationActive={false}
+                        dataKey="count_candidates_boys_pct"
                         stackId="a"
-                        fill="#8884d8"
-                    />
+                        fill="#a8dadc"
+                        name="% de candidats"
+                    >
+                        <LabelList
+                            position="middle"
+                            fill="#000"
+                            stroke="none"
+                            dataKey="label_candidates_boys"
+                        />
+                    </Bar>
                     <Bar
-                        dataKey="count_candidates_girls"
+                        isAnimationActive={false}
+                        dataKey="count_candidates_girls_pct"
                         stackId="a"
-                        fill="#82ca9d"
-                    />
+                        fill="#e63946"
+                        name="% de candidates"
+                    >
+                        <LabelList
+                            position="middle"
+                            fill="#000"
+                            stroke="none"
+                            dataKey="label_candidates_girls"
+                        />
+                    </Bar>
                     <Bar
-                        dataKey="count_accepted_boys"
+                        isAnimationActive={false}
+                        dataKey="count_accepted_boys_pct"
                         stackId="b"
-                        fill="#a88408"
-                    />
+                        fill="#76d169"
+                        name="% d'acceptés"
+                    >
+                        <LabelList
+                            position="middle"
+                            fill="#000"
+                            stroke="none"
+                            dataKey="label_accepted_boys"
+                        />
+                    </Bar>
                     <Bar
-                        dataKey="count_accepted_girls"
+                        isAnimationActive={false}
+                        dataKey="count_accepted_girls_pct"
                         stackId="b"
-                        fill="#a2cadd"
-                    />
+                        fill="#6b6fdb"
+                        name="% d'acceptées"
+                    >
+                        <LabelList
+                            position="middle"
+                            fill="#000"
+                            stroke="none"
+                            dataKey="label_accepted_girls"
+                        />
+                    </Bar>
                 </BarChart>
             </ResponsiveContainer>
         </StatsCard>
--- src/utils/generateFormationName.ts
+++ src/utils/generateFormationName.ts
@@ -7,7 +7,7 @@ export const generateFormationName = (
     ${getField(formation, 'g_ea_lib_vx')} 
     ${
         getField(formation, 'detail_forma') !== 'NaN'
-            ? -getField(formation, 'detail_forma')
+            ? `- ${getField(formation, 'detail_forma')}`
             : ''
     }`
 }
